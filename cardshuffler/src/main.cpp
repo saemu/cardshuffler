c365ad0567687e237f476a8eec71071ae9409654
Samuel Brand <saemu@users.noreply.github.com>
2017-08-31 17:13:53 +0200

    initial commit

---
+++ cardshuffler/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+* RUAG Schweiz AG
+* RUAG Aviation
+* Boenigstrasse 17
+* CH - 3812 Wilderswil - Switzerland
+*
+* Copyright (C) 2015, RUAG Schweiz AG, All rights reserved.
+*
+*/
+
+#include <iostream>
+
+int main( int argc, char* argv[] )
+{
+   std::cout << "Ace of Spades beats king of clubs..." << std::endl;
+
+   return 0;
+}
