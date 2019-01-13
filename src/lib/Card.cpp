3c48ff3d5cf278ad3a3e4600d87809a5754f59cd
Samuel Brand <saemu@users.noreply.github.com>
2019-01-13 13:45:27 +0100

    fix error C2679

--- src/lib/Card.cpp
+++ src/lib/Card.cpp
@@ -5,58 +5,60 @@
 #include "Card.hpp"
 
 #include <map>
+#include <string>
 
 namespace {
-    const std::map<Card::Rank, std::string> RankStrings{{Card::Rank::eAce,   "Ace"},
-                                                        {Card::Rank::eKing,  "King"},
-                                                        {Card::Rank::eQueen, "Queen"},
-                                                        {Card::Rank::eJack,  "Jack"},
-                                                        {Card::Rank::eTen,   "Ten"},
-                                                        {Card::Rank::eNine,  "Nine"},
-                                                        {Card::Rank::eEight, "Eight"},
-                                                        {Card::Rank::eSeven, "Seven"},
-                                                        {Card::Rank::eSix,   "Six"},};
+// clang-format off
+static const std::map<Card::Rank, std::string> RankStrings{{Card::Rank::eAce,   "Ace"},
+                                                           {Card::Rank::eKing,  "King"},
+                                                           {Card::Rank::eQueen, "Queen"},
+                                                           {Card::Rank::eJack,  "Jack"},
+                                                           {Card::Rank::eTen,   "Ten"},
+                                                           {Card::Rank::eNine,  "Nine"},
+                                                           {Card::Rank::eEight, "Eight"},
+                                                           {Card::Rank::eSeven, "Seven"},
+                                                           {Card::Rank::eSix,   "Six"}};
 
-    const std::map<Card::Suit, std::string> SuitStrings{{Card::Suit::eClubs,    "Clubs"},
-                                                        {Card::Suit::eDiamonds, "Diamonds"},
-                                                        {Card::Suit::eHearts,   "Hearts"},
-                                                        {Card::Suit::eSpades,   "Spades"},};
+static const std::map<Card::Suit, std::string> SuitStrings{{Card::Suit::eClubs,    "Clubs"},
+                                                           {Card::Suit::eDiamonds, "Diamonds"},
+                                                           {Card::Suit::eHearts,   "Hearts"},
+                                                           {Card::Suit::eSpades,   "Spades"}};
 
+// clang-format on
 
-    std::ostream& operator<<(std::ostream& pOut, const Card::Rank& pRank) {
-        auto rankString = RankStrings.find(pRank);
-        if (rankString == std::end(RankStrings)) {
-            throw std::invalid_argument("Given Rank is invalid");
-        }
+std::ostream &operator<<(std::ostream &pOut, const Card::Rank &pRank) {
+  auto rankString = RankStrings.find(pRank);
+  if (rankString == std::end(RankStrings)) {
+    throw std::invalid_argument("Given Rank is invalid");
+  }
 
-        pOut << rankString->second;
+  pOut << rankString->second;
 
-        return pOut;
-    }
+  return pOut;
+}
 
-    std::ostream& operator<<(std::ostream& pOut, const Card::Suit pSuit) {
-        auto suitString = SuitStrings.find(pSuit);
-        if (suitString == std::end(SuitStrings)) {
-            throw std::invalid_argument("Given Suit is invalid");
-        }
+std::ostream &operator<<(std::ostream &pOut, const Card::Suit pSuit) {
+  auto suitString = SuitStrings.find(pSuit);
+  if (suitString == std::end(SuitStrings)) {
+    throw std::invalid_argument("Given Suit is invalid");
+  }
 
-        pOut << suitString->second;
+  pOut << suitString->second;
 
-        return pOut;
-    }
+  return pOut;
+}
 
 } // namespace
 
-std::ostream& operator<<(std::ostream& pOut, const Card& pCard) {
-    pOut << pCard.mRank << " of " << pCard.mSuit;
-    return pOut;
+std::ostream &operator<<(std::ostream &pOut, const Card &pCard) {
+  pOut << pCard.mRank << " of " << pCard.mSuit;
+  return pOut;
 }
 
-bool operator==(const Card& left, const Card& right) noexcept {
-    return left.mRank == right.mRank && left.mSuit == right.mSuit;
+bool operator==(const Card &left, const Card &right) noexcept {
+  return left.mRank == right.mRank && left.mSuit == right.mSuit;
 }
 
 bool Card::isValid() const {
-    return mSuit != Suit::eUnknown && mRank != Rank::eUnknown;
-
+  return mSuit != Suit::eUnknown && mRank != Rank::eUnknown;
 }
