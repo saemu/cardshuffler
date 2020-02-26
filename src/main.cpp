f41aaf1b8fc3f44a8a0222c813ddd98fd42d8945
Samuel Brand <saemu@users.noreply.github.com>
2020-02-26 18:34:30 +0100

    use spdlog to print messages on console

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,24 @@
 #include "lib/Deck.hpp"
 
 #include <map>
-#include <string>
+#include <spdlog/fmt/ostr.h>
+#include <spdlog/spdlog.h>
 
 namespace {
-
-// clang-format off
-    const std::map<std::string, bool> validInputs{{"y",   true,},
-                                                  {"yes", true,},
-                                                  {"n",   false},
-                                                  {"no",  false},};
-// clang-format on
-
 bool readAnswer() {
-    std::string lQuit;
-    std::cout << "new shuffle [y/n]: ";
-    std::cin >> lQuit;
-
-    auto answer = validInputs.find(lQuit);
+    // clang-format off
+    static const std::map<std::string, bool> validInputs{{"y",   true,},
+                                                         {"yes", true,},
+                                                         {"n",   false},
+                                                         {"no",  false},};
+    // clang-format on
+    std::string quit;
+    spdlog::info("new shuffle [y/n]: ");
+    std::cin >> quit;
+    auto answer = validInputs.find(quit);
     if (answer == std::end(validInputs)) {
         throw std::invalid_argument("your answer is wrong, exiting");
     }
-
     return answer->second;
 }
 
@@ -34,24 +31,20 @@ bool readAnswer() {
 
 int main(int, char**) {
     try {
-        Deck lDeck;
-
+        spdlog::set_pattern("%v");
+        Deck deck;
         bool running{true};
-
         do {
-            lDeck.shuffle();
+            deck.shuffle();
             do {
-                Card lCard = lDeck.drawNextCard();
-                std::cout << "Card " << lCard << std::endl;
-            } while (lDeck.hasNext());
-
+                const Card lCard = deck.drawNextCard();
+                spdlog::info("Card {}", lCard);
+            } while (deck.hasNext());
             running = readAnswer();
-
         } while (running);
-    } catch (std::exception& ex) {
-        std::cerr << ex.what() << std::endl;
+    } catch (const std::logic_error& ex) {
+        spdlog::error("Logical error: {}", ex.what());
         return EXIT_FAILURE;
     }
-
     return EXIT_SUCCESS;
 }
