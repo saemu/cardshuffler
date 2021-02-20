8fbfd75cf0bca17c73b4e31f0314ce4b3b681377
Samuel Brand <saemu@users.noreply.github.com>
2021-02-20 13:17:00 +0100

    fix code smells detected by sonarcloud.io

--- tests/module/main.cpp
+++ tests/module/main.cpp
@@ -2,6 +2,6 @@
 #include <ApprovalTests.hpp>
 
 using namespace ApprovalTests;
-
-auto directoryDisposer = Approvals::useApprovalsSubdirectory("approval");
-auto frontLoadedReportDisposer = Approvals::useAsFrontLoadedReporter(BlockingReporter::onMachineNamed("CIRCLECI"));
+namespace {
+const auto directoryDisposer = Approvals::useApprovalsSubdirectory("approval");
+}  // namespace
